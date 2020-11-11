c84c1e03fa61f0c668be0aa65fe1b09c21c0acc2
cskone <cskone@my.hpu.edu>
2020-11-10 18:50:11 -1000

    various variable name changes, badgeID optional

--- pages/models.py
+++ pages/models.py
@@ -8,28 +8,28 @@ from django.utils import timezone
 
 class Guest(models.Model):
     name = models.CharField(max_length=30)
-    badge_id = models.CharField(max_length=30)
+    badge_id = models.CharField(max_length=30, blank=True)
     checked_in = models.BooleanField()
 
     def __str__(self):
         return '%s     |     BadgeID: %s     |      Checked In: %s' % (self.name, self.badge_id, str(self.checked_in))
 # Create your models here.
 
-
+# Meeting_title = Meeting_room
 class Meeting(models.Model):
     guest = models.ManyToManyField(Guest)
-    meeting_title = models.CharField(max_length=30)
+    meeting_room = models.CharField(max_length=30)
     meeting_desc = models.CharField(max_length=200)
     time_slot = models.DateTimeField()
 
-    def getMeetingTitle(self):
-        return self.meeting_title
+    def getMeetingRoom(self):
+        return self.meeting_room
 
     def getMeetingDesc(self):
         return self.meeting_desc
 
     def getMeetingInfo(self):
-        return '%s %s' % (self.meeting_title, self.meeting_desc)
+        return '%s %s' % (self.meeting_room, self.meeting_desc)
 
     def __str__(self):
-        return '%s at %s' % (self.meeting_title, self.time_slot.strftime("%c"))
+        return '%s at %s' % (self.meeting_room, self.time_slot.strftime("%c"))
